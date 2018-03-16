57563bb637a10de571f51832bd217432e3793738
Bruno Caldeira <brunocaldeira@outlook.com>
2018-03-16 16:40:36 +0000

    Push

---
+++ js/lib.js
@@ -0,0 +1,8 @@
+$(document).ready(function(e) {
+  $(".share-btn").click(function() {
+    $(".products-wrapper").toggleClass("show");
+  })  
+  $(".close").click(function() {
+    $(".products-wrapper").toggleClass("show");
+  })
+})
